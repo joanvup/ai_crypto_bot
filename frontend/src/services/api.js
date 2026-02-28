ad826423b5e8f90b0577a3741fe991d487268f85
Joan Fuentes <joanvup@gmail.com>
2026-02-28 12:34:15 -0500

    feat: add API service with functions for fetching bot status, balance, and trade data.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8006/api/v1';
+const API_URL = 'https://traderbot.colegiobilingue.edu.co/api/v1';
 
 export const getBotStatus = async () => {
     try {
